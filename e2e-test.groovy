2e3f509e528e45eebb36030cc6817b78c7e54605
ThushanLJ <thushanlakshitha97@gmail.com>
2020-10-09 15:47:17 +0530

    groovy-update

--- e2e-test.groovy
+++ e2e-test.groovy
@@ -1,9 +1,13 @@
 pipeline {
     agent any
     stages {
-        stage('Example') {
+        stage('Add jacoco agent') {
             steps {
                 bat 'java -javaagent:src/main/resources/jacocoagent.jar=destfile=target/jacoco-it.exec,append=true'
+            }
+        }
+        stage('Kill process') {
+            steps {
                 bat 'kill -15'
             }
         }
